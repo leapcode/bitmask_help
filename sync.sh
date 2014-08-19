a78cc510c39944aa5eefa59a0ac779e3dc43ebce
elijah <elijah@riseup.net>
2014-08-19 13:33:34 -0700

    added comment to sync.sh

--- sync.sh
+++ sync.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+#
+# Some of the pages used in bitmask.net are copies of pages used on dl.bitmask.net
+# Run this to copy those pages from ../dl_bitmask_net.git
+#
 for i in android linux mac windows
 do
   rsync -rv --delete ../dl_bitmask_net/pages/$i/ pages/install/$i
@@ -7,4 +10,3 @@ done
 
 rsync -rv ../dl_bitmask_net/pages/common/_download_button.haml pages/common
 rsync -rv ../dl_bitmask_net/pages/assets/qr/ pages/assets/qr
-
