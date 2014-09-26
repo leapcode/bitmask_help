8e0565b8238f68732344f8f9bc6eda0a6ce61a47
elijah <elijah@riseup.net>
2014-09-26 15:30:40 -0700

    imported latest dl.bitmask.net pages

--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@
 # Some of the pages used in bitmask.net are copies of pages used on dl.bitmask.net
 # Run this to copy those pages from ../dl_bitmask_net.git
 #
-for i in android linux mac windows
+for i in android linux mac windows signature-verification
 do
   rsync -rv --delete ../dl_bitmask_net/pages/$i/ pages/install/$i
 done
